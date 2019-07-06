ea73faf421cca129829e27b055c2c13fd858f53f
hucheng <hucheng@qutoutiao.net>
2019-07-06 15:36:14 +0800

    feat: init

---
+++ car.js
@@ -0,0 +1,13 @@
+const {
+	SyncHook,
+	SyncBailHook,
+	SyncWaterfallHook,
+	SyncLoopHook,
+	AsyncParallelHook,
+	AsyncParallelBailHook,
+	AsyncSeriesHook,
+	AsyncSeriesBailHook,
+	AsyncSeriesWaterfallHook
+ } = require("tapable");
+ const hook = new SyncHook(["arg1", "arg2", "arg3"]);
+
